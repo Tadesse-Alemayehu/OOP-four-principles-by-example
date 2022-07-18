746121e82993d079199dec54c43bf0c26b58f8b1
Tadesse-Alemayehu <email24t@gmail.com>
2022-07-18 20:42:04 +0300

    Add getter

--- animal.rb
+++ animal.rb
@@ -1,17 +1,38 @@
 class Animal
-  def initialize(number_of_legs, name = "Unknown")
+  def initialize(type, number_of_legs, name = "Unknown")
     @id = Random.rand(1..1000)
     @name = name
     @number_of_legs = number_of_legs
+    @type = type
   end
 
   def speak
-    "Bla bla bla"
+    puts "Bla bla bla"
+  end
+    def id
+    @id
+  end
+
+  def type
+    @type
+  end
+
+  def number_of_legs
+    @number_of_legs
+  end
+
+  def name
+    @name
+  end
+
+  def name=(value)
+    @name = value
   end
 end
 
-animal_1 = Animal.new(4, "Rex")
-animal_2 = Animal.new(8)
+animal_1 = Animal.new(4,4, "Rex")
+animal_2 = Animal.new(8,3)
 
 animal_1.speak
-animal_2.speak
+puts animal_2.id;
+
